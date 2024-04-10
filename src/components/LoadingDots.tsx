89bfc23aad919234505a4250ea56b62d8b41992c
Backend <118852056+Andre-Luiz22@users.noreply.github.com>
2024-04-10 16:14:53 -0300

    add mais tecnologias, reduzindo o tamanho dos dots

--- src/components/LoadingDots.tsx
+++ src/components/LoadingDots.tsx
@@ -1,9 +1,9 @@
 export function LoadingDots() {
   return (
     <div className=" flex gap-1 pt-2 items-center">
-      <div className="w-3 h-3 rounded-full animate-bounce bg-main-l dark:bg-main-d [animation-delay:-0.3s]"></div>
-      <div className="w-3 h-3 rounded-full animate-bounce bg-main-l dark:bg-main-d [animation-delay:-0.15s]"></div>
-      <div className="w-3 h-3 rounded-full animate-bounce bg-main-l dark:bg-main-d"></div>
+      <div className="w-2 h-2 rounded-full animate-bounce bg-main-l dark:bg-main-d [animation-delay:-0.2s]"></div>
+      <div className="w-2 h-2 rounded-full animate-bounce bg-main-l dark:bg-main-d [animation-delay:-0.15s]"></div>
+      <div className="w-2 h-2 rounded-full animate-bounce bg-main-l dark:bg-main-d"></div>
     </div>
   );
 }
